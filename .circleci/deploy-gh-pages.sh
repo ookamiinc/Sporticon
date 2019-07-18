d351d89a547857163df015acc62f6b3bed9b95f8
Riomar Mccartney <riomarmccartney@gmail.com>
2019-07-18 22:57:01 +0900

    Add CI config

---
+++ .circleci/deploy-gh-pages.sh
@@ -0,0 +1,15 @@
+git config user.name "$USER_NAME"
+git config user.email "$USER_EMAIL"
+
+git checkout gh-pages
+git pull origin gh-pages
+
+find . -maxdepth 1 ! -name 'docs' ! -name '.git' ! -name '.gitignore' -exec rm -rf {} \;
+mv docs/_site/* .
+rm -R docs
+
+git add -fA
+git commit --allow-empty -m "$(git log develop -1 --pretty=%B)"
+git push origin gh-pages
+
+echo "deployed successfully"
